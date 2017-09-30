609da5cafb500fee8ccdbf7ab136ee47b94e4b3e
fattaneh <fatameri@hotmail.com>
2017-09-29 20:52:59 -0400

    substring

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,15 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        static int Solve(string opr)
         {
+            // Complete this function
+            // Complete this function
+
+            var x = Int32.Parse(opr.Substring(0, 1));
+            var y = Int32.Parse(opr.Substring(2,1));
+            var result2 = 0;
+            var z = opr.Substring(1, 1);
+            if (x < 0 || y > 9) return result2;
+            switch (z)
+            {
+                case "+":
+                    result2 = x + y;
+                    break;
+                case "-":
+                    result2 = x - y;
+                    break;
+            }
+            return result2;
+        }
+
+        static void Main()
+        {
+            string opr = Console.ReadLine();
+            int result = Solve(opr);
+            Console.WriteLine(result);
         }
     }
 }
+
